f410dfe345fe64c2b73c9e5e04b16ef37eed4d82
Ankur <ankur13019@iiitd.ac.in>
2014-09-07 00:06:05 +0530

    ui changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,14 +6,32 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     # add custom create logic here
-    super
+    build_resource(sign_up_params)
     hex = SecureRandom.hex(3)
     while User.find_by_uid(hex)
     	hex = SecureRandom.hex(3)
     end
     resource.uid = hex
-    resource.save!
-    puts hex
+    resource_saved = resource.save
+    yield resource if block_given?
+    if resource_saved
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      @validatable = devise_mapping.validatable?
+      if @validatable
+        @minimum_password_length = resource_class.password_length.min
+      end
+      respond_with resource
+    end
   end
 
   def update
